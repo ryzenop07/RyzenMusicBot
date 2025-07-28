30b43319cb36fa1fafa4550b3e1643ae3dcb88c9
ryzenop07 <ajay211st@gmail.com>
2025-07-28 17:00:46 +0530

    Update config.py

--- config.py
+++ config.py
@@ -22,7 +22,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 # Chat id of a group for logging bot's activities
 import os
 print("LOG_GROUP_ID =", os.getenv("LOG_GROUP_ID"))
-LOG_GROUP_ID =-1002593671416
+LOG_GROUP_ID = -1002593671416
 
 # Get this value from @ultron2_robot on Telegram by /id
 OWNER_ID = 7850496630
