0bc18f378023583902eaa429e2d7655b030de8bc
ryzenop07 <ajay211st@gmail.com>
2025-07-28 14:57:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ MONGO_DB_URI = "mongodb+srv://ajay211st:vish123@musicbot.e8zz328.mongodb.net/?re
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
 
 # Chat id of a group for logging bot's activities
-LOG_GROUP_ID = -1002593671416
+LOG_GROUP_ID =-1002593671416
 
 # Get this value from @ultron2_robot on Telegram by /id
 OWNER_ID = 7850496630
